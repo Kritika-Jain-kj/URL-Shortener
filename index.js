dd2bbc743dfe271df5162db96a68e04560f68283
Kritika Jain <kritikajain2630@gmail.com>
2024-06-11 00:41:44 +0530

    Authentication

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express()
 const path = require('path')
 const urlRoute = require('./routes/url')
 const staticRoute = require('./routes/staticRouter')
+const userRoute = require('./routes/user')
 const URL = require('./models/url')
+const cookieParser = require('cookie-parser')
 const { connectToMongoDB } = require('./connect')
+const { restrictToLoggedinUserOnly, checkAuth } = require('./middleware/auth')
 
 connectToMongoDB('mongodb://localhost:27017/url-shortener').then(() =>
   console.log('MongoDB Connected !')
@@ -14,10 +17,12 @@ app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
 
 app.use(express.json())
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/url', urlRoute)
-app.use('/', staticRoute)
+app.use('/url', restrictToLoggedinUserOnly, urlRoute)
+app.use('/user', userRoute)
+app.use('/', checkAuth, staticRoute)
 
 app.get('/url/:shortId', async (req, res) => {
   const shortId = req.params.shortId
