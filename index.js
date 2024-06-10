518480eb644132653e669525d2aa2960dc0c50c9
Kritika Jain <kritikajain2630@gmail.com>
2024-06-10 18:11:25 +0530

    Added UI and SSR

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const urlRoute = require('./routes/url')
+const staticRoute = require('./routes/staticRouter')
 const URL = require('./models/url')
 const { connectToMongoDB } = require('./connect')
 
@@ -8,11 +10,16 @@ connectToMongoDB('mongodb://localhost:27017/url-shortener').then(() =>
   console.log('MongoDB Connected !')
 )
 
+app.set('view engine', 'ejs')
+app.set('views', path.resolve('./views'))
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/url', urlRoute)
+app.use('/', staticRoute)
 
-app.get('/:shortId', async (req, res) => {
+app.get('/url/:shortId', async (req, res) => {
   const shortId = req.params.shortId
   const entry = await URL.findOneAndUpdate(
     {
