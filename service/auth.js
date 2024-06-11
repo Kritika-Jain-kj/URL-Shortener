668333ed8b49d8fd7bfc7d6cc6064c1f1d932038
Kritika Jain <kritikajain2630@gmail.com>
2024-06-11 21:47:37 +0530

    jwt Authentication

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,35 @@
-const sessionIdToUserMap = new Map()
+//Statefull
+//const sessionIdToUserMap = new Map();
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user)
+// function setUserId(id, user) {
+//   sessionIdToUserMap.set(id, user);
+// }
+
+// function getUserId(id) {
+//   return sessionIdToUserMap.get(id);
+// }
+// + Necessary Changes in Other folders
+
+//Stateless
+const jwt = require('jsonwebtoken')
+const secret = 'Kritika#2630'
+
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  )
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id)
+function getUser(token) {
+  try {
+    return jwt.verify(token, secret)
+  } catch (error) {
+    return null
+  }
 }
 
 module.exports = {
